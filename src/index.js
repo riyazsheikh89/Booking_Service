3621c352f235d9f8b2b0c8d41ea3d2a1ba04f43f
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-01-28 19:33:40 +0530

    added API-Gateway

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ const setupAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    app.use('/api', apiRoutes);
+    // app.get('/bookingservice/api/v1/home', (req, res) => {
+    //     return res.json({message: 'hitting the booking service'});
+    // })
+
+    app.use('/bookingservice/api', apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server started on PORT: ${PORT}`);
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,5 +3,10 @@ const router = express.Router();
 
 const {BookingController} = require('../../controllers/index');
 router.post('/bookings', BookingController.create);
+router.get('/info', (req, res) => {
+    return res.json({
+        message: 'response from routes'
+    })
+})
 
 module.exports = router;
